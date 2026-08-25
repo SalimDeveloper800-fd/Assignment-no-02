80e55214cd3153bed6b54964d0b5a0838e55da74
SalimDeveloper800-fd <salimsohail142@gmail.com>
2026-08-25 20:59:26 +0500

    update

--- main.dart
+++ main.dart
@@ -34,4 +34,30 @@ if(percentage>=75){
 else{
   print("students not allowed in exam");
 }
+//question no 04
+int year=2024;
+
+if(year% 400 == 0){
+ print("leapyear");
 }
+else if(year% 100 == 0){
+  print("not a leap year");
+}
+else if(year% 4 == 0){
+ print("leap year");
+}
+else{
+  print("not a leap year");
+}
+
+}
+
+
+
+
+
+
+
+
+
+
